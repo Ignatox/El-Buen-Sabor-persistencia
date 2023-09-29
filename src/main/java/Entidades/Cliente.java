b544b40229b38a9839e0c1686dd940119b7b3730
Gonzalo Pozzoli <gonzalopozzoli2@gmail.com>
2023-09-29 09:57:30 -0300

    cliente creado

---
+++ src/main/java/Entidades/Cliente.java
@@ -0,0 +1,32 @@
+package Entidades;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+@Entity
+@Table(name="cliente")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cliente extends BaseEntidad{
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "apellido")
+    private String apellido;
+
+    @Column(name = "telefono")
+    private String telefono;
+
+    @Column(name = "email")
+    private String email;
+
+    // A agregar cuando creen las otras clases
+    //@OneToMany(mappedBy = "cliente",cascade = CascadeType.PERSIST)
+    //private List<Domicilio> domicilios = new ArrayList<Domicilio>();
+
+    //@OneToMany(mappedBy = "cliente", cascade = CascadeType.PERSIST)
+    //private List<Pedido> pedidos = new ArrayList<Pedido>();
+}
