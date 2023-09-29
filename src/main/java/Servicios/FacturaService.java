67f2a35f8d98ec69614293245621a557806a38c6
Niklasss98 <nlucca98@gmail.com>
2023-09-29 20:10:36 -0300

    FacturaService con metodos

--- src/main/java/Servicios/FacturaService.java
+++ src/main/java/Servicios/FacturaService.java
@@ -1,33 +1,82 @@
 package Servicios;
 
 import Entidades.Factura;
+import Repositorios.FacturaRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
+@Service
 public class FacturaService implements BaseService<Factura> {
 
+
+    private final FacturaRepository facturaRepository;
+    @Autowired
+    public FacturaService (FacturaRepository facturaRepository) {       //Inyección de Constructor (Es mejor que usar solo @Autowired, si no arranca cambiar)
+        this.facturaRepository = facturaRepository;
+    }
+
     @Override
+    @Transactional
     public List<Factura> findAll() throws Exception {
-        return null;
+        try {
+            List<Factura> entities = facturaRepository.findAll();
+            return entities;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
     }
 
     @Override
+    @Transactional
     public Factura findById(Long id) throws Exception {
-        return null;
+        try {
+            Optional<Factura> entityOptional = facturaRepository.findById(id);
+            return entityOptional.get();
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
     }
 
     @Override
-    public Factura save(Factura entidad) throws Exception {
-        return null;
+    @Transactional
+    public Factura save(Factura entity) throws Exception {
+        try {
+            entity = facturaRepository.save(entity);
+            return entity;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
     }
 
     @Override
-    public Factura update(Long id, Factura entidad) throws Exception {
-        return null;
+    @Transactional
+    public Factura update(Long id, Factura entity) throws Exception {
+        try {
+            Optional<Factura> entityOptional = facturaRepository.findById(id);
+            Factura factura = entityOptional.get();                             //No va a largar error pero puede que sea inestable
+            factura = facturaRepository.save(factura);
+            return factura;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
     }
 
     @Override
+    @Transactional
     public boolean delete(Long id) throws Exception {
-        return false;
+        try {
+            if(facturaRepository.existsById(id)) {
+                facturaRepository.deleteById(id);
+                return true;
+            } else {
+                throw new Exception();
+            }
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
     }
 }
