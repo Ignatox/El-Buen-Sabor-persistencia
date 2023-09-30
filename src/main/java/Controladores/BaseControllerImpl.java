53f3ebbae6822893fcf93d73ceef473ece18f602
Niklasss98 <nlucca98@gmail.com>
2023-09-30 20:20:53 -0300

    FFFFFF

--- src/main/java/Controladores/BaseControllerImpl.java
+++ src/main/java/Controladores/BaseControllerImpl.java
@@ -3,10 +3,14 @@ package Controladores;
 import Entidades.BaseEntidad;
 import Servicios.BaseServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.io.Serializable;
 
 public abstract class BaseControllerImpl<E extends BaseEntidad, S extends BaseServiceImpl<E, Long>> implements BaseController<E, Long> {
 @Autowired
     protected S servicio;
+
 }
