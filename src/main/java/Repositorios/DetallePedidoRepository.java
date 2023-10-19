2545eb4bcd5f3684d813ecc78bd8e6c7d2537ca9
Daniela Illanes <illanesdaniela28@gmail.com>
2023-10-18 22:41:22 -0300

    borrando el club

--- src/main/java/Repositorios/DetallePedidoRepository.java
+++ src/main/java/Repositorios/DetallePedidoRepository.java
@@ -1,28 +1,8 @@
 package Repositorios;
 
 import Entidades.DetallePedido;
-import Entidades.Pedido;
-import Entidades.Producto;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface DetallePedidoRepository extends BaseRepository<DetallePedido, Long>{
-
-    @Query("SELECT d FROM DetallePedido d WHERE d.cantidad = :cantidad")
-    List<Pedido> buscarPorCantidad(@Param("cantidad") int cantidad);
-
-    @Query("SELECT d FROM DetallePedido d WHERE d.cantidad = :cantidad")
-    Page<Pedido> buscarPorCantidad(@Param("cantidad") int cantidad, Pageable pageable);
-
-    @Query("SELECT d FROM DetallePedido d WHERE d.Producto = :producto")
-    List<Pedido> buscarPorProducto(@Param("producto") Producto producto);
-
-    @Query("SELECT d FROM DetallePedido d WHERE d.Producto = :producto")
-    Page<Pedido> buscarPorProducto(@Param("producto") Producto producto, Pageable pageable);
 }
--- src/main/java/Repositorios/RubroRepository.java
+++ src/main/java/Repositorios/RubroRepository.java
@@ -1,18 +1,8 @@
 package Repositorios;
 
 import Entidades.Rubro;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface RubroRepository extends BaseRepository <Rubro, Long>{
-
-    @Query("SELECT r FROM Rubro r WHERE r.denominacion = :denominacion")
-    List<Rubro> buscarPorDenominacion(@Param("denominacion") String denominacion);
-
-    @Query("SELECT r FROM Rubro r WHERE r.productos.denominacion = :denominacionProducto")
-    Rubro buscarPorDenominacionProducto(@Param("denominacionProducto") String denominacionProducto);
 }
