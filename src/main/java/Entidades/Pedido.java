b049884b98cae6286720e955e8952de460981fbd
MartinaLuconi <martinaluconi21@gmail.com>
2023-10-22 22:28:22 -0300

    Definición del estado Inicial de los pedido

---
+++ src/main/java/Entidades/Pedido.java
@@ -0,0 +1,38 @@
+package Entidades;
+
+import Enumeraciones.EstadoPedido;
+import Enumeraciones.TipoEnvio;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.antlr.v4.runtime.misc.NotNull;
+
+import java.util.Date;
+
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="pedido")
+
+public class Pedido extends BaseEntidad {
+    @Column(name = "fechapedido")
+    private String fecha;
+    @Column(name = "horaestimadaentrega")
+    @Temporal(TemporalType.TIMESTAMP)
+    private String horaEstimadaEntrega;
+    @Column(name = "total")
+    private double total;
+    @NotNull
+    @Column(name = "estadopedido")
+    @Enumerated(EnumType.STRING)
+    private EstadoPedido estado= EstadoPedido.A_CONFIRMAR; //define el estado inicial del pedido
+    @NotNull
+    @Column(name = "tipoenviopedido")
+    @Enumerated(EnumType.STRING)
+    private TipoEnvio tipoEnvio;
+}
--- src/main/java/com/utn/EBS/Enumeraciones/EstadoPedido.java
+++ src/main/java/com/utn/EBS/Enumeraciones/EstadoPedido.java
@@ -3,6 +3,7 @@ import java.util.EnumSet;
 import java.util.Set;
 
 public enum EstadoPedido {  //seguramente hay que agregar mas estados apra tener coherencia
+    A_CONFIRMAR,
     INICIADO,
     PREPARACION,
     ENTREGADO,
