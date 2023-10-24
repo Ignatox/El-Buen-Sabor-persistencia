c29304fad7a101c2e1055685d183784abd853c16
Gonzalo Pozzoli <gonzalopozzoli2@gmail.com>
2023-10-24 20:52:12 -0300

    repositorio rubro y detallePedido corregidos

--- src/main/java/com/utn/EBS/Repositorios/DetallePedidoRepository.java
+++ src/main/java/com/utn/EBS/Repositorios/DetallePedidoRepository.java
@@ -14,14 +14,14 @@ import java.util.List;
 @Repository
 public interface DetallePedidoRepository extends BaseRepository<DetallePedido, Long>{
     @Query("SELECT d FROM DetallePedido d WHERE d.cantidad = :cantidad")
-    List<Pedido> buscarPorCantidad(@Param("cantidad") int cantidad);
+    List<DetallePedido> buscarPorCantidad(@Param("cantidad") int cantidad);
 
     @Query("SELECT d FROM DetallePedido d WHERE d.cantidad = :cantidad")
-    Page<Pedido> buscarPorCantidad(@Param("cantidad") int cantidad, Pageable pageable);
+    Page<DetallePedido> buscarPorCantidad(@Param("cantidad") int cantidad, Pageable pageable);
 
-    @Query("SELECT d FROM DetallePedido d WHERE d.Producto = :producto")
-    List<Pedido> buscarPorProducto(@Param("producto") Producto producto);
+    @Query("SELECT d FROM DetallePedido d WHERE d.producto = :producto")
+    List<DetallePedido> buscarPorProducto(@Param("producto") Producto producto);
 
-    @Query("SELECT d FROM DetallePedido d WHERE d.Producto = :producto")
+    @Query("SELECT d FROM DetallePedido d WHERE d.producto = :producto")
     Page<Pedido> buscarPorProducto(@Param("producto") Producto producto, Pageable pageable);
 }
--- src/main/java/com/utn/EBS/Repositorios/RubroRepository.java
+++ src/main/java/com/utn/EBS/Repositorios/RubroRepository.java
@@ -12,6 +12,4 @@ public interface RubroRepository extends BaseRepository <Rubro, Long>{
     @Query("SELECT r FROM Rubro r WHERE r.denominacion = :denominacion")
     List<Rubro> buscarPorDenominacion(@Param("denominacion") String denominacion);
 
-    @Query("SELECT r FROM Rubro r WHERE r.productos.denominacion = :denominacionProducto")
-    Rubro buscarPorDenominacionProducto(@Param("denominacionProducto") String denominacionProducto);
 }
