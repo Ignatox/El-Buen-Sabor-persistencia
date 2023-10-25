ba13afb6d9e954d02c42bb08e89b5bc732e506e2
Daniela Illanes <illanesdaniela28@gmail.com>
2023-10-25 09:25:28 -0300

    buscar Rubros por denominacion, servicio y controlador

--- src/main/java/com/utn/EBS/Controladores/RubroController.java
+++ src/main/java/com/utn/EBS/Controladores/RubroController.java
@@ -2,12 +2,30 @@ package com.utn.EBS.Controladores;
 
 import com.utn.EBS.Entidades.Rubro;
 import com.utn.EBS.Servicios.RubroServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping(path = "api/v1/rubros")
 public class RubroController extends BaseControllerImpl<Rubro, RubroServiceImpl>{
+
+    @Autowired
+    private RubroServiceImpl rubroServiceImpl;
+    @GetMapping("/Denominacion")
+    public ResponseEntity<?> buscarRubrosPorDenominacion(String denominacion) throws Exception{
+        try {
+            List<Rubro> rubrosPorDenominacion = rubroServiceImpl.buscarPorDenominacion(denominacion);
+            return ResponseEntity.status(HttpStatus.OK).body(rubrosPorDenominacion);
+        }catch(Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"error\":\"Error. Intente despues.\"}");
+        }
+    }
 }
--- src/main/java/com/utn/EBS/Servicios/RubroServiceImpl.java
+++ src/main/java/com/utn/EBS/Servicios/RubroServiceImpl.java
@@ -6,6 +6,8 @@ import com.utn.EBS.Repositorios.RubroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class RubroServiceImpl extends BaseServiceImpl<Rubro, Long> implements RubroService{
     @Autowired
@@ -13,5 +15,14 @@ public class RubroServiceImpl extends BaseServiceImpl<Rubro, Long> implements Ru
 
     public RubroServiceImpl(BaseRepository<Rubro, Long> baseRepository) {
         super(baseRepository);
+    }
+
+    public List<Rubro> buscarPorDenominacion(String denominacion) throws Exception {
+        try{
+            List<Rubro> rubrosPorDenominacion = rubroRepository.buscarPorDenominacion(denominacion);
+            return rubrosPorDenominacion;
+        } catch(Exception e){
+            throw new Exception(e.getMessage());
+        }
     }
 }
