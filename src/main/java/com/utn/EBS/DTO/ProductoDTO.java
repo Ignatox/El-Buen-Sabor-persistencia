a82d3cf36367d692cbc7cbae18bb5ff6d0a25535
Daniela Illanes <illanesdaniela28@gmail.com>
2023-11-05 16:33:13 -0300

    Update ProductoDTO.java

--- src/main/java/com/utn/EBS/DTO/ProductoDTO.java
+++ src/main/java/com/utn/EBS/DTO/ProductoDTO.java
@@ -6,11 +6,21 @@ import com.utn.EBS.Entidades.ProductoIngrediente;
 import java.util.List;
 
 public class ProductoDTO {
+
+    Long idProducto;
     int cantidad;
     int tiempoEstimadoCocina;
+<<<<<<< Updated upstream
     String nombre;
     String descripcion;
     String foto;
     List<ProductoIngrediente> ingredientes;
+=======
+    String denominacion;
+    String receta;
+
+    String foto;
+    String nombre;
+>>>>>>> Stashed changes
 }
 
