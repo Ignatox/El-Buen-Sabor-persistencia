81978ba70e6b492586699a96ce8b23a7e627b47f
Niklasss98 <nlucca98@gmail.com>
2023-11-07 16:58:53 -0300

    NotaCredito

---
+++ src/main/java/com/utn/EBS/Entidades/NotaCredito.java
@@ -0,0 +1,15 @@
+package com.utn.EBS.Entidades;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Table(name="NotaCredito")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class NotaCredito extends BaseEntidad{
+
+}
