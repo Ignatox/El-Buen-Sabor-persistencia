f7d5e5b3dfd1bff9916e3cbd2803980b2c0fa3df
Daniela Illanes <illanesdaniela28@gmail.com>
2023-11-08 15:53:30 -0300

    Create IngredienteDTO.java

---
+++ src/main/java/com/utn/EBS/DTO/IngredienteDTO.java
@@ -0,0 +1,14 @@
+package com.utn.EBS.DTO;
+
+import com.utn.EBS.Enumeraciones.UnidadMedida;
+
+public class IngredienteDTO {
+
+    String nombre;
+    Double costo;
+    int stockActual;
+
+    int stockMinimo;
+
+    UnidadMedida unidadMedida;
+}
