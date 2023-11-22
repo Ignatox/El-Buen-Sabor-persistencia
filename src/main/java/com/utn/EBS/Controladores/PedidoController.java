1fd59cb8a5f3d122e4751c5a6ecefe2dae1277d8
MartinaLuconi <martinaluconi21@gmail.com>
2023-11-22 11:02:45 -0300

    ..

--- src/main/java/com/utn/EBS/Controladores/PedidoController.java
+++ src/main/java/com/utn/EBS/Controladores/PedidoController.java
@@ -88,21 +88,6 @@ public class PedidoController extends BaseControllerImpl<Pedido, PedidoServiceIm
         try{
             return ResponseEntity.status(HttpStatus.OK).body(pedidoService.buscarPedidosAEntregar(pageable));
         }catch (Exception e) {
-
-    @GetMapping("/EntregarPedidos")
-    public ResponseEntity<?> buscarPedidosAEntregar(Pageable pageable ){
-        try {
-            return ResponseEntity.status(HttpStatus.OK).body(pedidoService.buscarPorCliente(id));
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-        }
-    }
-
-    @GetMapping("/EntregarPedidos") //delivery
-    public ResponseEntity<?> buscarPedidosAEntregar(Pageable pageable){
-        try{
-            return ResponseEntity.status(HttpStatus.OK).body(pedidoService.buscarPedidosAEntregar(pageable));
-        }catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
     }
